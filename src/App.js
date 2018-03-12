44667c7800aeb9c1dd18f3c734feeee454fd92cc
Masa <masa.sippola@gmail.com>
2018-03-12 19:30:23 +0200

    Add bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar } from 'react-bootstrap';
 import './App.css';
 
 import ReportPage from './ReportPage.js';
@@ -11,11 +11,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <div> <ReportPage apiUrl={this.apiUrl}/></div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <h1 className="App-title">Home</h1>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <div> <ReportPage apiUrl={this.apiUrl}/></div>
       </div>
     );
   }
