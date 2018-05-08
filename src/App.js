af3e192d72c886496536092c5282184bf7b72063
Tangeni <kamatitangenisks@gmail.com>
2018-05-08 17:48:48 +0200

    user auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 import ReportPage from './ReportPage.js';
 
+import Login from './Authenticate/Login.js';
+import ForgotPassword from './Authenticate/ForgotPassword.js';
+import EditUser from './Admin/EditUser.js';
+import ECBLogo from './Images/ECBLogo.png';
+
 class App extends Component {
     apiUrl='https://ecb.guelland.eu';
     // uncomment the following line if you want to test a local backend.
@@ -12,10 +17,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <img src={ECBLogo} className="App-logo" alt="logo" />
+          <br/>
+          <h1 className="App-title">Welcome to Electricity Control Board</h1>
+          <br/>
         </header>
-          <div> <ReportPage apiUrl={this.apiUrl}/></div>
+          {/*<div> <ReportPage apiUrl={this.apiUrl}/></div>*/}
+          <div> <Login apiUrl={this.apiUrl} /></div>
+          <div> <ForgotPassword apiUrl={this.apiUrl} /></div>
+          <div> <EditUser apiUrl={this.apiUrl} /></div>
       </div>
     );
   }
